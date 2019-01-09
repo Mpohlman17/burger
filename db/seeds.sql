cbd234ecb42ce672eba2b7cfbb27a521949dea62
Michael Pohlman <michael.pohlman1@gmail.com>
2019-01-08 22:11:02 -0500

    fully functional, working out some kinks and improving final work

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+-- INSERT INTO burgers (
+--     burger_name,
+--     devoured
+-- ) VALUES
+-- (
+--     'King Burger',
+--     0
+-- ),
+-- (
+--     'Chicken Bacon Ranch',
+--     0
+-- ),
+-- (
+--     'Big Texas Burger',
+--     0
+-- );
+
+INSERT INTO burgers (burger_name) VALUES ('King Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Bacon Ranch');
+INSERT INTO burgers (burger_name) VALUES ('Big Texas Burger');
